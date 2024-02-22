e3f95db56b4ae313bbfa82682459cdad2a1cd339
rmoura584 <rmoura584@gmail.com>
2024-02-22 01:18:14 +0000

    commit

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
+import CalculatorButton from './CalculatorButton';
+import './Calculator.css';
+
+
 
 function Calculator() {
   const [input, setInput] = useState('');
 
-  const handleInput = (value) => {
-    setInput(input + value);
+  const handleInput = (e) => {
+    setInput(input + e.target.name);
   };
 
   const calculate = () => {
@@ -29,27 +33,46 @@ function Calculator() {
         <input type="text" value={input} readOnly />
       </form>
       <div className="keypad">
-        <button onClick={clear} id="clear">Limpar</button>
-        <button onClick={backspace} id="backspace">C</button>
-        <button name="/" onClick={handleInput}>&divide;</button>
-        <button name="7" onClick={handleInput}>7</button>
-        <button name="8" onClick={handleInput}>8</button>
-        <button name="9" onClick={handleInput}>9</button>
-        <button name="*" onClick={handleInput}>&times;</button>
-        <button name="4" onClick={handleInput}>4</button>
-        <button name="5" onClick={handleInput}>5</button>
-        <button name="6" onClick={handleInput}>6</button>
-        <button name="-" onClick={handleInput}>&ndash;</button>
-        <button name="1" onClick={handleInput}>1</button>
-        <button name="2" onClick={handleInput}>2</button>
-        <button name="3" onClick={handleInput}>3</button>
-        <button name="+" onClick={handleInput}>+</button>
-        <button name="0" onClick={handleInput}>0</button>
-        <button name="." onClick={handleInput}>.</button>
-        <button onClick={calculate} id="result">=</button>
+        {/* Números de 1 a 9 */}
+        <div className="numbers">
+          <button name="7" onClick={handleInput}>7</button>
+          <button name="8" onClick={handleInput}>8</button>
+          <button name="9" onClick={handleInput}>9</button>
+          <button name="4" onClick={handleInput}>4</button>
+          <button name="5" onClick={handleInput}>5</button>
+          <button name="6" onClick={handleInput}>6</button>
+          <button name="1" onClick={handleInput}>1</button>
+          <button name="2" onClick={handleInput}>2</button>
+          <button name="3" onClick={handleInput}>3</button>
+        </div>
+
+        {/* Zero e ponto decimal */}
+        <div className="bottom-row">
+        </div>
+
+        {/* Operações */}
+        <div className="operations">
+         
+          
+        </div>
+
+        {/* Controles */}
+        <div className="controls">
+          <button name="0" onClick={handleInput}>0</button>
+          <button name="." onClick={handleInput}>.</button>
+          <button name="+" onClick={handleInput}>+</button>
+          <button name="-" onClick={handleInput}>-</button>
+          <button name="*" onClick={handleInput}>&times;</button>
+          <button name="/" onClick={handleInput}>&divide;</button>
+          <button onClick={clear} id="clear">Limpar</button>
+          <button onClick={backspace} id="backspace">C</button>
+          <button onClick={calculate} id="result">=</button>
+          
+        </div>
       </div>
     </div>
   );
 }
+
 
 export default Calculator;
---
+++ src/CalculatorButton.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react';
+
+function CalculatorButton({ label, onClick }) {
+  return (
+    <button onClick={() => onClick(label)}>{label}</button>
+  );
+}
+
+export default CalculatorButton;
